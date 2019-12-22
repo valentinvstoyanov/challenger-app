7e83e73957048c9855e92e582c388a4cdd9e3dc5
valentinvstoyanov <valentinstoqnov98@gmail.com>
2019-12-22 19:25:54 +0200

    Add basic register ui screen

--- challenger/lib/register.dart
+++ challenger/lib/register.dart
@@ -1,23 +1,38 @@
-import 'package:challenger/register.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget {
+class RegisterScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return new _LoginScreenState();
+    return new _RegisterScreenState();
   }
 }
 
-class _LoginScreenState extends State<LoginScreen> {
-  final fieldStyle = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
-
+class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
+    final fieldStyle = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
     final emailField = TextField(
       style: fieldStyle,
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
           hintText: "Email",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0))),
+    );
+
+    final usernameField = TextField(
+      style: fieldStyle,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Username",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0))),
+    );
+
+    final nameField = TextField(
+      style: fieldStyle,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Name",
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0))),
     );
 
@@ -30,6 +45,21 @@ class _LoginScreenState extends State<LoginScreen> {
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0))),
     );
 
+    final registerButton = Material(
+      elevation: 4.0,
+      borderRadius: BorderRadius.circular(8.0),
+      color: Colors.amber,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: Text("Sign up",
+            textAlign: TextAlign.center,
+            style: fieldStyle.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
     final loginButton = Material(
       elevation: 4.0,
       borderRadius: BorderRadius.circular(8.0),
@@ -37,26 +67,10 @@ class _LoginScreenState extends State<LoginScreen> {
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
-        child: Text("Sign in",
-            textAlign: TextAlign.center,
-            style: fieldStyle.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
-    );
-
-    final registerButton = Material(
-      elevation: 4.0,
-      borderRadius: BorderRadius.circular(8.0),
-      color: Colors.amberAccent,
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
         onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => RegisterScreen()));
+          Navigator.pop(context);
         },
-        child: Text("Sign up",
+        child: Text("Sign in",
             textAlign: TextAlign.center,
             style: fieldStyle.copyWith(
                 color: Colors.white, fontWeight: FontWeight.bold)),
@@ -83,15 +97,19 @@ class _LoginScreenState extends State<LoginScreen> {
                 SizedBox(height: 45.0),
                 emailField,
                 SizedBox(height: 25.0),
+                usernameField,
+                SizedBox(height: 25.0),
+                nameField,
+                SizedBox(height: 25.0),
                 passwordField,
                 SizedBox(
                   height: 35.0,
                 ),
-                loginButton,
+                registerButton,
                 SizedBox(
                   height: 25.0,
                 ),
-                registerButton,
+                loginButton,
                 SizedBox(
                   height: 15.0,
                 ),
