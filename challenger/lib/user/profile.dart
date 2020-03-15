09e4ee715fb406d5a1b6ded365ed62c6c55ea73b
valentinvstoyanov <valentinstoqnov98@gmail.com>
2020-03-15 17:26:32 +0200

    Add better looking profile page

--- challenger/lib/user/profile.dart
+++ challenger/lib/user/profile.dart
@@ -61,7 +61,165 @@ class _ProfilePageState extends State<ProfilePage> {
       ),
       body: ListView(
         children: <Widget>[
-          Text(user == null ? "" : user.username)
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 20.0, right: 10.0, top: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundImage: NetworkImage('https://i1.wp.com/www.nationalreview.com/wp-content/uploads/2020/03/vladimir-putin-erdogan.jpg?fit=789%2C460&ssl=1'),
+                    ),
+                    FlatButton(
+                        onPressed: () {},
+                        child: Text("Edit Profile")
+                    ),
+                  ]
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+                child: Text('Vladimir Putin',
+                  style: Theme.of(context).textTheme.title,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left:20.0),
+                child: Text('Moscow, RU',
+                  style: Theme.of(context).textTheme.subtitle,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left:20.0, top: 20.0, right: 20.0),
+                child: Text('Hello, I am Vlad. I love making cool photos, beautiful architecture and icecream.',
+                  style: Theme.of(context).textTheme.body1,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text('1789',
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        Text('Followers',
+                          style: Theme.of(context).textTheme.overline,
+                        )
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text('236',
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        Text('Following',
+                          style: Theme.of(context).textTheme.overline,
+                        )
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text('990',
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        Text('Challenges',
+                          style: Theme.of(context).textTheme.overline,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 25.0),
+              Container(
+                padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                height: 200.0,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      height: 200.0,
+                      width: 200.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: DecorationImage(
+                              image: AssetImage('assets/picone.jpeg'),
+                              fit: BoxFit.cover
+                          )
+                      ),
+                    ),
+                    SizedBox(width: 10.0),
+                    Container(
+                      height: 200.0,
+                      width: 200.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: DecorationImage(
+                              image: AssetImage('assets/pictwo.jpeg'),
+                              fit: BoxFit.cover
+                          )
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Container(
+                padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                height: 100.0,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: DecorationImage(
+                              image: AssetImage('assets/picthree.jpeg'),
+                              fit: BoxFit.cover
+                          )
+                      ),
+                    ),
+                    SizedBox(width: 10.0),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: DecorationImage(
+                              image: AssetImage('assets/picfour.jpeg'),
+                              fit: BoxFit.cover
+                          )
+                      ),
+                    ),
+                    SizedBox(width: 10.0),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: DecorationImage(
+                              image: AssetImage('assets/picfive.jpeg'),
+                              fit: BoxFit.cover
+                          )
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 10.0),
+            ],
+          )
         ],
       ),
     );
