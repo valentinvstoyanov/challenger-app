9cf3bc67757ee0baeb1ff69ed4d1ff080f7f6572
valentinvstoyanov <valentinstoqnov98@gmail.com>
2020-08-23 17:07:31 +0300

    Add get challenge by id method request

--- challenger/lib/challenges/domain/challenge_api.dart
+++ challenger/lib/challenges/domain/challenge_api.dart
@@ -28,6 +28,17 @@ class ChallengeApi {
     throw Exception("Unexpected error occured while trying to create new challenge");
   }
 
+  Future<Challenge> getChallengeById(String id) async {
+    final headers = {HttpHeaders.authorizationHeader:userStore.getToken()};
+    final response = await http.get('$baseUrl/challenges/$id', headers: headers);
+
+    if (response.statusCode == HttpStatus.ok) {
+      return Challenge.fromJson(json.decode(response.body));
+    }
+
+    throw Exception("Unexpected error occured while trying to get challenge by id");
+  }
+
   Future<List<Challenge>> getAllChallenges() async {
     final headers = {HttpHeaders.authorizationHeader:userStore.getToken()};
     final response = await http.get('$baseUrl/challenges', headers: headers);
@@ -38,6 +49,6 @@ class ChallengeApi {
       return users;
     }
 
-    throw Exception("Unexpected error occured while trying to get all users");
+    throw Exception("Unexpected error occured while trying to get all challenges");
   }
 }
